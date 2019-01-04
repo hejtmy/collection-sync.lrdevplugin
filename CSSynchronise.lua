5e122dac14de683ad9d376f488ba7360ee135049
hejtmy <hejtmy@gmail.com>
2019-01-04 04:46:52 +0100

    Adds searching for folders

--- CSSynchronise.lua
+++ CSSynchronise.lua
@@ -1,5 +1,7 @@
 local LrLogger = import 'LrLogger'
 local LrDialogs = import 'LrDialogs'
+local LrFunctionContext = import 'LrFunctionContext'
+local LrTasks = import 'LrTasks'
 
 local syncLogger = LrLogger('syncLogger')
 syncLogger:enable( "print" ) -- Pass either a string or a table of actions.
@@ -10,6 +12,18 @@ end
 CSSynchronise = {}
 
 CSSynchronise.FolderToCollectionSync = function(folder, collection, recursive)
-	local str = folder .. collection .. recursive
-	LrDialogs.message( "Title", str, "info" );
+	LrTasks.startAsyncTask(function()
+		LrFunctionContext.callWithContext( "showCustomDialogWithMultipleBind", function( context )
+			LrDialogs.attachErrorDialogToFunctionContext( context )
+			local str = folder .. collection .. recursive
+			local catalog = import "LrApplication".activeCatalog()
+			local folders = catalog:getFolders()
+			local names = {}
+			for k, v in ipairs(folders) do
+				names[#names+1] = v:getName()
+			end
+			local message = table.concat(names,"\n")
+			LrDialogs.message( "Title", message, "info" );
+		end)
+	end)
 end
