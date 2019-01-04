da5188265a42a2174e9533624cb0054218537cd5
hejtmy <hejtmy@gmail.com>
2019-01-04 17:17:01 +0100

    Adds recurrsion checking

--- CSInit.lua
+++ CSInit.lua
@@ -1,6 +1,6 @@
 CSInit = {
-	DefaultFolder = "Photos/Boardgames/2017/2017-Davis",
-	DefaultCollection = "Boardgames/2017/2017-Davis",
+	DefaultFolder = "Photos/Boardgames/",
+	DefaultCollection = "Boardgames/",
 	recursive = 1,
 	TutorialURL = "https://github.com/hejtmy/collection-sync.lrdevplugin",
 }
--- CSSynchronise.lua
+++ CSSynchronise.lua
@@ -20,11 +20,40 @@ CSSynchronise.FolderToCollectionSync = function(folder, collection, recursive)
 		LrFunctionContext.callWithContext( "showCustomDialogWithMultipleBind", function( context )
 			local syncFolder = CSHelpers.findFolder(context, folder, recursive)
 			-- needs to establish, if the folder is the top level
-			local syncCollection = CSHelpers.findOrCreateCollectionTree(context, collection, false)
 			if syncFolder ~= nil then
-				message = "Folder present"
-				LrDialogs.message( "Title", message, "info" );
+				if recursive then
+					outputToLog(syncFolder:getPath())
+					CSSynchronise.RecursiveFolderSync(context, syncFolder, syncFolder:getPath(), collection)
+				else 
+					local syncCollection = CSHelpers.findOrCreateCollectionTree(context, collection, false)
+					CSSynchronise.CopyPhotosFromFolderToCollection(context, syncFolder, syncCollection)
+				end
 			end
 		end)
 	end)
+end
+
+CSSynchronise.CopyPhotosFromFolderToCollection = function(context, folder, collection)
+	local photos = folder:getPhotos(false) -- don't includeChildren
+	collection.catalog:withWriteAccessDo('copytingPhotosToCollection', function(context)
+		collection:addPhotos(photos)
+	end,
+	{timeout=3, callback=function() 
+		LrDialogs.message( "Title", "Photos failed to copy", "info" ) 
+	end})
+end
+
+CSSynchronise.RecursiveFolderSync = function(context, topFolder, rootFolderPath, rootCollectionPath)
+	outputToLog("folder name " .. topFolder:getPath())
+	local folders = topFolder:getChildren()
+	outputToLog("number of children" .. #folders)
+	if #folders == 0 then  -- if we are int he lowest folder
+		relativeFolderName = string.gsub(topFolder:getPath(), rootFolderPath, "")
+		outputToLog(relativeFolderName)
+		--relative to the topFolder
+	else -- else we recusively go deeper
+		for f = 1, #folders do
+			CSSynchronise.RecursiveFolderSync(context, folders[f], rootFolderPath, rootCollectionPath)
+		end
+	end
 end
